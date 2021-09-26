1e111beabd547a31bab5ba8aa1de02f487f0567d
khushmanvar <khushmanvar@gmail.com>
2021-09-26 23:39:54 +0530

    fix: test locally

--- oneGraphHelper.js
+++ oneGraphHelper.js
@@ -6,6 +6,8 @@ const {
 } = require('apollo-server-express')
 const { GraphQLNonNull } = require('graphql')
 // Deps
+const jwt = require('jsonwebtoken')
+var jwksClient = require('jwks-rsa')
 
 const atob = str => {
   return Buffer.from(str, 'base64').toString('binary')
